7c698e8ab9ab8ad82c39a6ebfd137d1f3232d677
James Taylor <jamest@xwing.btn1.brandwatch.net>
2015-07-17 16:07:02 +0100

    Improved initial code

--- slack-go.php
+++ slack-go.php
@@ -1,29 +1,37 @@
 <?php
 
-define("GOSERVER", "http://go.brandwatch.com/");
-define("GOSERVERSHORT", "http://go/");
-define("GOSERVERDISPLAY", "go");
-define("BOTNAME", "go");
-define("INCOMINGWEBHOOK", "https://hooks.slack.com/services/xxx"); //Use your own incoming webhook URL here
+define("GO_SERVER", "http://go.brandwatch.com/");
+define("GO_SERVER_SHORT", "http://go/");
+define("GO_SERVER_DISPLAY", "go");
+define("GO_LINKS_API_PATH", "_api/go-link.json");
+define("BOT_NAME", "Go/");
+define("INCOMING_WEBHOOK", "https://hooks.slack.com/services/xxx"); //Use your own incoming webhook URL here
 
-function get_long_url( $slug ) 
-{ 
+function getGoLinks ( $server ) //returns JSON array of linkd from Go/ server
+{
     $options = array( 
         CURLOPT_RETURNTRANSFER => true,     // return web page 
         CURLOPT_HEADER         => false,    // return headers 
     ); 
  
-    $ch = curl_init( GOSERVER . '_api/go-link.json' ); 
-    curl_setopt_array( $ch, $options ); 
-    $content = curl_exec( $ch ); 
+    $curlHandle = curl_init( $server ); 
+    curl_setopt_array( $curlHandle, $options ); 
+    $content = curl_exec( $curlHandle ); 
  
-    if(curl_errno($ch)) {
+
+    if (curl_errno($curlHandle)) {
         die ("An error occured (is the Go server up?)"); //problem with the go server
     }
 
-    curl_close( $ch ); 
+    curl_close( $curlHandle ); 
 
-    $links = json_decode($content, true);
+    return json_decode($content, true);
+
+}
+
+function getLongUrl( $slug )
+{ 
+    $links = getGoLinks( GO_SERVER . GO_LINKS_API_PATH );
 
     foreach ($links as $link) {
     	if ($link['shortUri'] == $slug) {
@@ -33,23 +41,32 @@ function get_long_url( $slug )
     die ("No link found for " . $slug);
 }
 
-$long_url = get_long_url( $_POST["text"] );
+function postToSlack( $message )
+{
+    $payload = array(
+        text => $message,
+        username => BOT_NAME,
+        channel => $_POST["channel_id"],
+        link_names => 1,
+        mrkdwn => "true"
+    );
 
-// do the incoming webhook
-$payload = array(
-    text => "@" . $_POST["user_name"] . " shared *<" . GOSERVERSHORT . $_POST["text"] . "|" . GOSERVERDISPLAY . "/" . $_POST["text"] . ">*\n" . "_ <" . GOSERVERSHORT . $_POST["text"] . "|" . urlencode($long_url) . ">_",
-    username => BOTNAME,
-    channel => $_POST["channel_id"],
-    link_names => 1,
-    mrkdwn => "true"
-);
+    $json_payload = json_encode($payload);
 
-$json_payload = json_encode($payload);
+    $curlHandle = curl_init();
+    curl_setopt($curlHandle, CURLOPT_URL, INCOMING_WEBHOOK);
+    curl_setopt($curlHandle, CURLOPT_POST, 1);
+    curl_setopt($curlHandle, CURLOPT_POSTFIELDS, "payload=" . $json_payload);
+    curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
 
-$ch = curl_init();
-curl_setopt($ch,CURLOPT_URL, INCOMINGWEBHOOK);
-curl_setopt($ch,CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, "payload=" . $json_payload);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($curlHandle);
 
-$result = curl_exec($ch);
+}
+
+$longUrl = getLongUrl( $_POST["text"] );
+
+$returnText = "@" . $_POST["user_name"] . " shared *<" . 
+              GO_SERVER_SHORT . $_POST["text"] . "|" . GO_SERVER_DISPLAY . "/" . $_POST["text"] . ">*\n" . 
+              "_ <" . GO_SERVER_SHORT . $_POST["text"] . "|" . urlencode($longUrl) . ">_";
+
+postToSlack($returnText);
